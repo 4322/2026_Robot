aebdf24b25ce819876c1df8e6a4d17f6848813e8
Ellie Morse <elliemorse@frc4322.com>
2026-01-24 11:05:57 -0800

    Added shooting subsystem

---
+++ src/main/java/frc/robot/subsystems/drive/Shooting/AreaManager/AreaManager.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.drive.Shooting.AreaManager;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AreaManager extends SubsystemBase {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/drive/Shooting/Shooting.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.drive.Shooting;
+
+public class Shooting expand Subsystembase{
+    
+}
