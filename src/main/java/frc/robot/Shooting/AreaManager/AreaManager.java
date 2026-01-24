c9891c4da450100ae54696b9954ffeaf6c683e56
Ellie Morse <elliemorse@frc4322.com>
2026-01-24 12:13:33 -0800

    Added method to check if we are in shooting area

--- src/main/java/frc/robot/Shooting/AreaManager/AreaManager.java
+++ src/main/java/frc/robot/Shooting/AreaManager/AreaManager.java
@@ -1,3 +1,24 @@
 package frc.robot.Shooting.AreaManager;
 
-public class AreaManager {}
+import edu.wpi.first.math.geometry.Rectangle2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.subsystems.Drive.Drive;
+
+public class AreaManager {
+  private Drive drive;
+
+  private AreaManager(Drive drive) {
+    this.drive = drive;
+  }
+
+  Translation2d drivePosition = drive.getPose().getTranslation();
+
+  // Red
+  Rectangle2d trenchLeftRed = new Rectangle2d(new Translation2d(0, 0), new Translation2d(0, 0));
+
+  Rectangle2d trenchRightRed = new Rectangle2d(new Translation2d(0, 0), new Translation2d(0, 0));
+
+  public boolean inNoShootingArea() {
+    return trenchLeftRed.contains(drivePosition) || trenchRightRed.contains(drivePosition);
+  }
+}
