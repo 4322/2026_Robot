bf376225b2c2f66dc24a1d6b025ca17512e8594e
Ellie Morse <elliemorse@frc4322.com>
2026-01-24 15:16:32 -0800

    #newSyntax States

--- src/main/java/frc/robot/subsystems/shooter.java/Shooting.java
+++ src/main/java/frc/robot/subsystems/shooter.java/Shooting.java
@@ -2,4 +2,35 @@ package frc.robot.subsystems.Shooter.java;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooting extends SubsystemBase {}
+public class Shooting extends SubsystemBase {
+    private enum ShooterState {
+        DISABLED,
+        UNWINDING,
+        SCORING,
+        PASSING,
+        INHIBIT
+
+    }
+
+    ShooterState currentState = ShooterState.DISABLED;
+
+    private void periodic() {
+       switch (currentState) {
+            case DISABLED ->{
+
+            }
+            case UNWINDING -> {
+
+            }
+            case SCORING ->  {
+
+            }
+            case PASSING -> {
+
+            }
+            case INHIBIT -> {
+
+            }
+        }
+    }
+}
