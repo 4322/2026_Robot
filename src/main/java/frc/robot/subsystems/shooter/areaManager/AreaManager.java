13eb61cbd8e39aef81249208ea3913054ce2c3f6
Ryan Yeung <ryanyeung@frc4322.com>
2026-01-25 10:49:00 -0800

    Move areaManager

--- src/main/java/frc/robot/subsystems/shooter/areaManager/AreaManager.java
+++ src/main/java/frc/robot/subsystems/shooter/areaManager/AreaManager.java
@@ -1,4 +1,4 @@
-package frc.robot.util.areaManager;
+package frc.robot.subsystems.shooter.areaManager;
 
 import edu.wpi.first.math.geometry.Rectangle2d;
 import edu.wpi.first.math.geometry.Translation2d;
