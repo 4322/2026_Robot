295e239c164fda6bb5f7ce2a859fe22e0261e88a
Ryan Yeung <ryanyeung@frc4322.com>
2026-01-24 16:33:10 -0800

    Remove requests Change logging

--- src/main/java/frc/robot/subsystems/shooter/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/shooter/spindexer/Spindexer.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.shooter.spindexer;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import org.littletonrobotics.junction.Logger;
@@ -27,34 +28,34 @@ public class Spindexer extends SubsystemBase {
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Spindexer", inputs);
-    Logger.recordOutput("Spindexer/State", state.toString());
+    
 
     switch (state) {
       case DISABLED -> {
-        if (requestIdle) {
+        if (DriverStation.isEnabled()) {
           state = SpindexerStates.IDLE;
-          unsetAllRequests();
         }
       }
       case IDLE -> {
         io.stop();
-        if (requestIndex) {
-          state = SpindexerStates.INDEXING;
-          unsetAllRequests();
-        }
       }
       case INDEXING -> {
         io.setTargetVelocity(Constants.Spindexer.indexingVelocityRotationsPerSec);
-        if (requestIdle) {
-          state = SpindexerStates.IDLE;
-          unsetAllRequests();
-        }
       }
     }
+
+    Logger.recordOutput("Spindexer/State", state.toString());
   }
 
-  private void unsetAllRequests() {
-    requestIdle = false;
-    requestIndex = false;
+  public void requestIdle() {
+    state = SpindexerStates.IDLE;
+  }
+
+  public void requestIndex() {
+    state = SpindexerStates.INDEXING;
+  }
+
+  public void enableBrakeMode(boolean enable) {
+    io.enableBrakeMode(enable);
   }
 }
