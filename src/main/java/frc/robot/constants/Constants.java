cf988d9c4d89b808294484430d1a948e3eaacf0d
Ellie Morse <elliemorse@frc4322.com>
2026-02-01 14:14:20 -0800

    Constants merge

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -1,14 +1,10 @@
-// Copyright (c) 2021-2026 Littleton Robotics
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by a BSD
-// license that can be found in the LICENSE file
-// at the root directory of this project.
+
 
 package frc.robot.constants;
 
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -44,7 +40,9 @@ public final class Constants {
 
   public enum SubsystemMode {
     DISABLED,
-    NORMAL
+    NORMAL,
+    TUNING,
+    DRIVE_TUNING
   }
 
   public static final SubsystemMode driveMode = SubsystemMode.NORMAL;
@@ -56,7 +54,9 @@ public final class Constants {
   public static final SubsystemMode turretMode = SubsystemMode.NORMAL;
   public static final SubsystemMode deployerMode = SubsystemMode.NORMAL;
   public static final SubsystemMode rollerMode = SubsystemMode.NORMAL;
+  public static final SubsystemMode intakeMode = SubsystemMode.NORMAL;
   public static final SubsystemMode climberMode = SubsystemMode.DISABLED;
+  public static final SubsystemMode ledMode = SubsystemMode.NORMAL;
   public static final SubsystemMode visionGlobalPose = SubsystemMode.NORMAL;
   public static final SubsystemMode visionObjectDetection = SubsystemMode.NORMAL;
 
@@ -69,18 +69,18 @@ public final class Constants {
     public static final NeutralModeValue neutralMode = NeutralModeValue.Brake;
     public static final double kS = 0;
     public static final double kV = 0;
-    public static final double kP = 0;
+    public static final double kP = 1;
     public static final double kI = 0;
     public static final double kD = 0;
-    public static final double indexingVelocityRotationsPerSec = 0;
-    public static final double stoppedThreshold = 0.1; // TODO (Mechanism rotations)
+    public static final double indexingMechanismRotationsPerSec = 3; // TODO 
+    public static final double stoppedMechanismRotationsPerSec = 0.1; // TODO
 
-    public static final double motorToMechanismRatio = 1.0; // TODO
+    public static final double motorToMechanismRatio = 10.0; // TODO
   }
 
   public static class Tunnel {
 
-    public static final double indexingVelocityRotationsPerSec = 0;
+    public static final double indexingMotorRotationsPerSec = 10;
     public static final int tunnelMotorId = 0;
     public static final double statorCurrentLimit = 60; // TODO
     public static final double supplyCurrentLimit = 40; // TODO
@@ -89,12 +89,12 @@ public final class Constants {
     public static final NeutralModeValue neutralMode = NeutralModeValue.Brake;
     public static final double kS = 0;
     public static final double kV = 0;
-    public static final double kP = 0;
+    public static final double kP = 1;
     public static final double kI = 0;
     public static final double kD = 0;
 
-    public static final double stoppedThreshold = 0.1; // TODO (Mechanism rotations)
-    public static final double atSpeedThreshold = 5; // TODO (Mechanism rotations)
+    public static final double stoppedMechanismRotationsPerSec = 0.1; // TODO
+    public static final double atSpeedMechanismRotationsPerSec = 0.95 * indexingMotorRotationsPerSec; // TODO
     public static final double motorToMechanismRatio = 1.0; // TODO
   }
 
@@ -122,4 +122,14 @@ public final class Constants {
     public static final double atSpeedThreshold = 5; // TODO (Mechanism rotations)
     public static final double motorToMechanismRatio = 1.0; // TODO
   }
+
+  public static class VisionObjectDetection {
+
+    public static final Transform3d robotCenterToCamera = new Transform3d(); // TODO add
+    public static final String hostname = null;
+  }
+
+
+
+  
 }
