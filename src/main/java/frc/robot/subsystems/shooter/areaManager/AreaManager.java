7b3ad9a51ab0ea737c3c0046a03248c9307e9a83
Ellie Morse <elliemorse@frc4322.com>
2026-02-05 17:01:52 -0800

    Added Trenches

--- src/main/java/frc/robot/subsystems/shooter/areaManager/AreaManager.java
+++ src/main/java/frc/robot/subsystems/shooter/areaManager/AreaManager.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.shooter.areaManager;
 
 import edu.wpi.first.math.geometry.Rectangle2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.Robot;
 
@@ -11,35 +12,56 @@ public class AreaManager {
   // All dimensions are in meters
   // Alliance Zones
   private static Rectangle2d redAllianceZone =
-      new Rectangle2d(new Translation2d(12.51, 8.07), new Translation2d(16.54, 0));
+      new Rectangle2d(
+          new Translation2d(12.51, Units.inchesToMeters(317.69)),
+          new Translation2d(Units.inchesToMeters(651.22), 0));
   private static Rectangle2d blueAllianceZone =
-      new Rectangle2d(new Translation2d(0, 8.07), new Translation2d(4.03, 0));
+      new Rectangle2d(
+          new Translation2d(0, Units.inchesToMeters(317.69)),
+          new Translation2d(Units.inchesToMeters(158.61), 0));
 
   // Neutral Zones
   private static Rectangle2d leftNeutralZone =
       new Rectangle2d(
-          new Translation2d(4.03, 8.07),
-          new Translation2d(11.22, 4.035)); // When looking from Blue Alliance side
+          new Translation2d(4.03, Units.inchesToMeters(317.69)),
+          new Translation2d(
+              11.22, Units.inchesToMeters(158.84))); // When looking from Blue Alliance side
   private static Rectangle2d rightNeutralZone =
       new Rectangle2d(
-          new Translation2d(4.03, 4.035),
+          new Translation2d(4.03, Units.inchesToMeters(158.84)),
           new Translation2d(11.22, 0)); // When looking from Blue Alliance side
   // Opposition Zones
   private static Rectangle2d leftBlueOppositionZone =
-      new Rectangle2d(new Translation2d(0, 8.07), new Translation2d(4.03, 4.035));
+      new Rectangle2d(
+          new Translation2d(0, Units.inchesToMeters(317.69)),
+          new Translation2d(Units.inchesToMeters(158.61), Units.inchesToMeters(158.84)));
   private static Rectangle2d rightBlueOppositionZone =
-      new Rectangle2d(new Translation2d(0, 4.035), new Translation2d(4.03, 0));
+      new Rectangle2d(
+          new Translation2d(0, Units.inchesToMeters(158.84)),
+          new Translation2d(Units.inchesToMeters(158.61), 0));
   private static Rectangle2d leftRedOppositionZone =
-      new Rectangle2d(new Translation2d(12.51, 8.07), new Translation2d(16.54, 4.035));
+      new Rectangle2d(
+          new Translation2d(12.51, Units.inchesToMeters(317.69)),
+          new Translation2d(Units.inchesToMeters(651.22), Units.inchesToMeters(158.84)));
   private static Rectangle2d rightRedOppositionZone =
-      new Rectangle2d(new Translation2d(12.51, 4.035), new Translation2d(16.54, 0));
+      new Rectangle2d(
+          new Translation2d(12.51, Units.inchesToMeters(158.84)),
+          new Translation2d(Units.inchesToMeters(651.22), 0));
 
   // Non-Shooting Areas
   private static Rectangle2d trenchLeftRed =
-      new Rectangle2d(new Translation2d(0, 4.1148), new Translation2d(8.2296, 8.2296));
+      new Rectangle2d(
+          new Translation2d(
+              (Units.inchesToMeters(47) - Units.inchesToMeters(12)), Units.inchesToMeters(317.69)),
+          new Translation2d(
+              (Units.inchesToMeters(47) + Units.inchesToMeters(12)), Units.inchesToMeters(255.1)));
   private static Rectangle2d trenchRightRed =
-      new Rectangle2d(new Translation2d(0, 8.2296), new Translation2d(8.2296, 12.3444));
-  private static Rectangle2d bumpLeftRed =
+      new Rectangle2d(
+          new Translation2d(
+              (Units.inchesToMeters(47) - Units.inchesToMeters(12)), Units.inchesToMeters(62.59)),
+          new Translation2d((Units.inchesToMeters(47) + Units.inchesToMeters(12)), 0));
+  
+private static Rectangle2d bumpLeftRed =
       new Rectangle2d(new Translation2d(0, 6.1722), new Translation2d(2.0574, 10.287));
   private static Rectangle2d bumpRightRed =
       new Rectangle2d(new Translation2d(0, 10.287), new Translation2d(2.0574, 14.4018));
