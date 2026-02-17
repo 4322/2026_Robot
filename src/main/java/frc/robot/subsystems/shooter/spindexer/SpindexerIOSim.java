f99d95999404f16b04c8fd6ec6455f300181cf1f
Ryan Yeung <ryanyeung@frc4322.com>
2026-02-17 07:59:37 -0800

    Spindexer and Tunnel Sims

--- src/main/java/frc/robot/subsystems/shooter/spindexer/SpindexerIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/spindexer/SpindexerIOSim.java
@@ -1,3 +1,37 @@
 package frc.robot.subsystems.shooter.spindexer;
 
-public class SpindexerIOSim {}
+public class SpindexerIOSim implements SpindexerIO {
+    private double rate = 0.2;
+    private double mechanismRotationsPerSec = 0.0;
+    private double targetVelocity = 0.0;
+
+    @Override
+    public void updateInputs(SpindexerIOInputs inputs) {
+        inputs.motorConnected = true;
+        simVelocity();
+        inputs.mechanismRotationsPerSec = mechanismRotationsPerSec;
+    }
+
+    @Override
+    public void setTargetMechanismRotations(double velocity) {
+        targetVelocity = velocity;
+    }
+
+    @Override
+    public void stop() {
+        targetVelocity = 0.0;
+    }
+
+    @Override
+    public void enableBrakeMode(boolean enable) {
+        return;
+    }
+
+    private void simVelocity() {
+        if (mechanismRotationsPerSec < targetVelocity) {
+            mechanismRotationsPerSec += rate;
+        } else if (mechanismRotationsPerSec > targetVelocity) {
+            mechanismRotationsPerSec -= rate;
+        }
+    }
+}
--- src/main/java/frc/robot/subsystems/shooter/tunnel/TunnelIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/tunnel/TunnelIOSim.java
@@ -1,3 +1,38 @@
 package frc.robot.subsystems.shooter.tunnel;
 
-public class TunnelIOSim {}
+public class TunnelIOSim implements TunnelIO {
+    private double rate = 0.2;
+    private double mechanismRotationsPerSec = 0.0;
+    private double targetVelocity = 0.0;
+
+    @Override
+    public void updateInputs(TunnelIOInputs inputs) {
+        inputs.motorConnected = true;
+        simVelocity();
+        inputs.mechanismRotationsPerSec = mechanismRotationsPerSec;
+    }
+
+    @Override
+    public void setTargetMechanismRotations(double velocity) {
+        targetVelocity = velocity;
+    }
+
+    @Override
+    public void stop() {
+        targetVelocity = 0.0;
+    }
+
+    @Override
+    public void enableBrakeMode(boolean enable) {
+        return;
+    }
+
+    private void simVelocity() {
+        if (mechanismRotationsPerSec < targetVelocity) {
+            mechanismRotationsPerSec += rate;
+        } else if (mechanismRotationsPerSec > targetVelocity) {
+            mechanismRotationsPerSec -= rate;
+        }
+    }
+
+}
