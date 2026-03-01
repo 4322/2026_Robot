08fa42e9d9e1f96c5592424f49c9d63f829f6acf
Doug Kavner <dougkavner@frc4322.com>
2026-02-28 18:01:55 -0800

    Force gyro heading to zero to test drive tuning

--- src/main/java/frc/robot/subsystems/drive/GyroIOBoron.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOBoron.java
@@ -5,6 +5,7 @@ import com.reduxrobotics.sensors.canandgyro.CanandgyroSettings;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.constants.Constants;
 
 public class GyroIOBoron implements GyroIO {
@@ -26,9 +27,15 @@ public class GyroIOBoron implements GyroIO {
   public void updateInputs(GyroIOInputs inputs) {
     // check connection with 60ms timeout instead of 2s default due to frequency of gyro reporting
     // failure
-    inputs.connected = gyro.isConnected(0.06);
+    inputs.connected = true;
     inputs.yawPosition = Rotation2d.fromRotations(gyro.getMultiturnYaw());
     inputs.yawVelocityRadPerSec = Units.rotationsToDegrees(gyro.getAngularVelocityYaw());
+    inputs.odometryYawTimestamps = new double[100];
+    inputs.odometryYawPositions = new Rotation2d[100];
+    for (int i = 0; i <= 99; i++) {
+      inputs.odometryYawTimestamps[i] = RobotController.getFPGATime() / 1e6;
+      inputs.odometryYawPositions[i] = inputs.yawPosition;
+    }
   }
 
   public Canandgyro getGyro() {
