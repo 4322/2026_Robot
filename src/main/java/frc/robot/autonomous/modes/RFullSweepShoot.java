ccb5ac186146c20f4761459dd242d717b4005521
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-10 09:20:26 -0700

    Replace .andThen() chaining with sequential command groups

--- src/main/java/frc/robot/autonomous/modes/RFullSweepShoot.java
+++ src/main/java/frc/robot/autonomous/modes/RFullSweepShoot.java
@@ -3,7 +3,6 @@ package frc.robot.autonomous.modes;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -11,8 +10,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.AutoIntake;
 import frc.robot.commands.IntakeCommands;
-import frc.robot.commands.ShooterCommands;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.led.LED;
@@ -38,13 +35,11 @@ public class RFullSweepShoot extends SequentialCommandGroup {
             }),
         new ParallelCommandGroup(
             IntakeCommands.setIntaking(intake),
-            AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake)
-                .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full))
-                .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Flip))
-                .andThen(AutoBuilder.followPath(Robot.R_Neutral_Mid_To_ShootR))
-                .andThen(ShooterCommands.aimAndShoot(shooter, drive))
-                .onlyWhile(() -> DriverStation.isAutonomous())
-                .onlyIf(() -> Constants.turretLocked)));
+            new SequentialCommandGroup(
+                AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake),
+                AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full),
+                AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Flip),
+                AutoBuilder.followPath(Robot.R_Neutral_Mid_To_ShootR))));
   }
 
   public RFullSweepShoot(
@@ -66,12 +61,12 @@ public class RFullSweepShoot extends SequentialCommandGroup {
               }
             }),
         new ParallelCommandGroup(IntakeCommands.setIntaking(intake)),
-        AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake)
-            .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full))
-            .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Flip))
-            .andThen(
-                new ParallelCommandGroup(
-                    new AutoIntake(drive, visionObjectDetection, led, intake, false),
-                    AutoBuilder.followPath(Robot.R_Neutral_Mid_To_ShootR))));
+        new SequentialCommandGroup(
+            AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake),
+            AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full),
+            AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Flip),
+            new ParallelCommandGroup(
+                new AutoIntake(drive, visionObjectDetection, led, intake, false),
+                AutoBuilder.followPath(Robot.R_Neutral_Mid_To_ShootR))));
   }
 }
--- src/main/java/frc/robot/autonomous/modes/RMidlineSweepShoot.java
+++ src/main/java/frc/robot/autonomous/modes/RMidlineSweepShoot.java
@@ -3,16 +3,12 @@ package frc.robot.autonomous.modes;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
-import frc.robot.commands.AutoIntake;
 import frc.robot.commands.IntakeCommands;
-import frc.robot.commands.ShooterCommands;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.led.LED;
@@ -38,13 +34,11 @@ public class RMidlineSweepShoot extends SequentialCommandGroup {
             }),
         new ParallelCommandGroup(
             IntakeCommands.setIntaking(intake),
-            AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake_Midline)
-                .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline))
-                .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline_Flip))
-                .andThen(AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR))
-                .andThen(ShooterCommands.aimAndShoot(shooter, drive))
-                .onlyWhile(() -> DriverStation.isAutonomous())
-                .onlyIf(() -> Constants.turretLocked)));
+            new SequentialCommandGroup(
+                AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake_Midline),
+                AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline),
+                AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline_Flip),
+                AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR))));
   }
 
   public RMidlineSweepShoot(
@@ -64,12 +58,11 @@ public class RMidlineSweepShoot extends SequentialCommandGroup {
               }
             }),
         new ParallelCommandGroup(IntakeCommands.setIntaking(intake)),
-        AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake_Midline)
-            .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline))
-            .andThen(AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline_Flip))
-            .andThen(
-                new ParallelCommandGroup(
-                    new AutoIntake(drive, visionObjectDetection, led, intake, false),
-                    AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR))));
+        new SequentialCommandGroup(
+            AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake_Midline),
+            AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline),
+            AutoBuilder.followPath(Robot.R_NeutralR_Intake_Full_Midline_Flip),
+            AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR),
+            AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR)));
   }
 }
