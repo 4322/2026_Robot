7520830d1db33aabc00b896af9a49fa04a98036f
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-17 20:17:33 -0700

    Paths smoother and less jerky Should hopefully not run into wall

--- src/main/java/frc/robot/autonomous/modes/RHalfSweepShoot.java
+++ src/main/java/frc/robot/autonomous/modes/RHalfSweepShoot.java
@@ -7,11 +7,9 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Robot;
 import frc.robot.commands.AutoIntake;
 import frc.robot.commands.IntakeCommands;
-import frc.robot.commands.ShooterCommands;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
@@ -61,17 +59,12 @@ public class RHalfSweepShoot extends SequentialCommandGroup {
                 }
               }),
           new ParallelCommandGroup(
+              IntakeCommands.setIntaking(intake),
               new SequentialCommandGroup(
-                  ShooterCommands.trenchOverride(hood).withTimeout(1),
-                  new WaitCommand(7),
-                  ShooterCommands.trenchOverride(hood).withTimeout(2)),
-              new ParallelCommandGroup(
-                  IntakeCommands.setIntaking(intake),
-                  new SequentialCommandGroup(
-                      AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake),
-                      AutoBuilder.followPath(Robot.R_NeutralR_Intake_To_Mid),
-                      AutoBuilder.followPath(Robot.R_NeutralR_Intake_Mid_Flip),
-                      AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR)))));
+                  AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake),
+                  AutoBuilder.followPath(Robot.R_NeutralR_Intake_To_Mid),
+                  AutoBuilder.followPath(Robot.R_NeutralR_Intake_Mid_Flip),
+                  AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR))));
     }
   }
 
