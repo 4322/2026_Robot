3cc76d910280b2fdc453a584a428f2907b95d409
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-19 22:19:02 -0700

    Build

--- src/main/java/frc/robot/autonomous/modes/LHalfSweepShoot.java
+++ src/main/java/frc/robot/autonomous/modes/LHalfSweepShoot.java
@@ -7,19 +7,13 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Robot;
-import frc.robot.commands.AutoIntake;
 import frc.robot.commands.IntakeCommands;
-import frc.robot.commands.ShooterCommands;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.led.LED;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.hood.Hood;
-import frc.robot.subsystems.vision.visionObjectDetection.VisionObjectDetection;
-import org.littletonrobotics.junction.Logger;
 
 public class LHalfSweepShoot extends SequentialCommandGroup {
 
@@ -29,24 +23,22 @@ public class LHalfSweepShoot extends SequentialCommandGroup {
     Pose2d startPoseRed = path.flipPath().getStartingHolonomicPose().get();
 
     setName("L_HALF_SWEEP_SHOOT");
-    
-      addCommands(
-          new InstantCommand(
-              () -> {
-                if (Robot.alliance == Alliance.Blue) {
-                  drive.setPose(startPoseBlue);
-                } else {
-                  drive.setPose(startPoseRed);
-                }
-              }),
-              new ParallelCommandGroup(
-                  IntakeCommands.setIntaking(intake),
-                  new SequentialCommandGroup(
-                      AutoBuilder.followPath(Robot.L_StartL_To_NeutralL_Intake),
-                      AutoBuilder.followPath(Robot.L_NeutralL_Intake_To_Mid),
-                      AutoBuilder.followPath(Robot.L_NeutralL_Intake_Mid_Flip),
-                      AutoBuilder.followPath(Robot.L_NeutralLMid_To_ShootL))));
-    
-  }
 
+    addCommands(
+        new InstantCommand(
+            () -> {
+              if (Robot.alliance == Alliance.Blue) {
+                drive.setPose(startPoseBlue);
+              } else {
+                drive.setPose(startPoseRed);
+              }
+            }),
+        new ParallelCommandGroup(
+            IntakeCommands.setIntaking(intake),
+            new SequentialCommandGroup(
+                AutoBuilder.followPath(Robot.L_StartL_To_NeutralL_Intake),
+                AutoBuilder.followPath(Robot.L_NeutralL_Intake_To_Mid),
+                AutoBuilder.followPath(Robot.L_NeutralL_Intake_Mid_Flip),
+                AutoBuilder.followPath(Robot.L_NeutralLMid_To_ShootL))));
+  }
 }
--- src/main/java/frc/robot/autonomous/modes/RHalfSweepShoot.java
+++ src/main/java/frc/robot/autonomous/modes/RHalfSweepShoot.java
@@ -7,19 +7,15 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Robot;
 import frc.robot.commands.AutoIntake;
 import frc.robot.commands.IntakeCommands;
-import frc.robot.commands.ShooterCommands;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.led.LED;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.hood.Hood;
 import frc.robot.subsystems.vision.visionObjectDetection.VisionObjectDetection;
-import org.littletonrobotics.junction.Logger;
 
 public class RHalfSweepShoot extends SequentialCommandGroup {
 
@@ -29,24 +25,23 @@ public class RHalfSweepShoot extends SequentialCommandGroup {
     Pose2d startPoseRed = path.flipPath().getStartingHolonomicPose().get();
 
     setName("R_HALF_SWEEP_SHOOT");
-    
-      addCommands(
-          new InstantCommand(
-              () -> {
-                if (Robot.alliance == Alliance.Blue) {
-                  drive.setPose(startPoseBlue);
-                } else {
-                  drive.setPose(startPoseRed);
-                }
-              }),
-              new ParallelCommandGroup(
-                  IntakeCommands.setIntaking(intake),
-                  new SequentialCommandGroup(
-                      AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake),
-                      AutoBuilder.followPath(Robot.R_NeutralR_Intake_To_Mid),
-                      AutoBuilder.followPath(Robot.R_NeutralR_Intake_Mid_Flip),
-                      AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR))));
-    
+
+    addCommands(
+        new InstantCommand(
+            () -> {
+              if (Robot.alliance == Alliance.Blue) {
+                drive.setPose(startPoseBlue);
+              } else {
+                drive.setPose(startPoseRed);
+              }
+            }),
+        new ParallelCommandGroup(
+            IntakeCommands.setIntaking(intake),
+            new SequentialCommandGroup(
+                AutoBuilder.followPath(Robot.R_StartR_To_NeutralR_Intake),
+                AutoBuilder.followPath(Robot.R_NeutralR_Intake_To_Mid),
+                AutoBuilder.followPath(Robot.R_NeutralR_Intake_Mid_Flip),
+                AutoBuilder.followPath(Robot.R_NeutralRMid_To_ShootR))));
   }
 
   public RHalfSweepShoot(
