02d09f88131092f6aa56fef5937bc84b84ace440
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-19 21:27:50 -0700

    Revert "Start importing 6328 hub util code"
    This reverts commit efc2bc21cfffdeb719c49439950c4b10de6157c1.

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -21,7 +21,6 @@ import frc.robot.subsystems.shooter.tunnel.Tunnel;
 import frc.robot.subsystems.shooter.turret.Turret;
 import frc.robot.subsystems.vision.visionGlobalPose.VisionGlobalPose;
 import frc.robot.util.GeomUtil;
-import frc.robot.util.HubShiftUtil;
 import org.littletonrobotics.junction.Logger;
 
 public class Shooter extends SubsystemBase {
