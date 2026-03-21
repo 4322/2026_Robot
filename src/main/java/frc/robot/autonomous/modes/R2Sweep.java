56b1395c60a334f16f4afbb1f357fb71d9cbd538
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-21 12:47:39 -0700

    Integrate new shooting changes

--- src/main/java/frc/robot/autonomous/modes/R2Sweep.java
+++ src/main/java/frc/robot/autonomous/modes/R2Sweep.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.IntakeCommands;
+import frc.robot.commands.ShooterCommands;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.led.LED;
@@ -36,11 +37,15 @@ public class R2Sweep extends SequentialCommandGroup {
             new SequentialCommandGroup(
                 AutoBuilder.followPath(Robot.R_2SWEEP_A),
                 AutoBuilder.followPath(Robot.R_2SWEEP_B),
+                ShooterCommands.toggleAutoShoot(shooter, true),
                 AutoBuilder.followPath(Robot.R_2SWEEP_CG),
+                ShooterCommands.toggleAutoShoot(shooter, false),
                 AutoBuilder.followPath(Robot.R_2SWEEP_D),
                 AutoBuilder.followPath(Robot.R_2SWEEP_E),
                 AutoBuilder.followPath(Robot.R_2SWEEP_F),
+                ShooterCommands.toggleAutoShoot(shooter, true),
                 AutoBuilder.followPath(Robot.R_2SWEEP_CG),
+                ShooterCommands.toggleAutoShoot(shooter, false),
                 AutoBuilder.followPath(Robot.R_2SWEEP_H))));
   }
 }
