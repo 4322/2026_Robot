0d03d53bac81bad391f868ee8e90b492e97b6d74
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-21 13:13:16 -0700

    Fix parameter

--- src/main/java/frc/robot/autonomous/AutonomousSelector.java
+++ src/main/java/frc/robot/autonomous/AutonomousSelector.java
@@ -75,7 +75,7 @@ public class AutonomousSelector {
     autos =
         List.of(
             new Auto(AutoName.DO_NOTHING, new DoNothing(hood)),
-            new Auto(AutoName.C_DEPOT_OUTPOST, new CDepotOutpost(drive, led, intake)),
+            new Auto(AutoName.C_DEPOT_OUTPOST, new CDepotOutpost(drive, led, intake, shooter)),
             new Auto(AutoName.C_OUTPOST, new COutpost(drive, led, shooter)),
             new Auto(AutoName.R_FULL_SWEEP_SHOOT, new RFullSweepShoot(drive, led, intake, shooter)),
             new Auto(
