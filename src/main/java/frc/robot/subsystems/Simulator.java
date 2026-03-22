d024732433865358c93051eb91c60b1f33d230d1
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-22 09:42:46 -0700

    Fix regression test

--- src/main/java/frc/robot/subsystems/Simulator.java
+++ src/main/java/frc/robot/subsystems/Simulator.java
@@ -238,8 +238,7 @@ public class Simulator extends SubsystemBase {
           new RegressionTest("Controller Test 1", TeleopScenario.CONTROLLER_TEST1, Alliance.Blue),
           new RegressionTest("Controller Test 2", TeleopScenario.CONTROLLER_TEST2, Alliance.Blue));
       case SUBSYSTEM_TEST_BOTH -> List.of(
-          new RegressionTest("Auto test", AutoName.L_HALF_SWEEP_SHOOT, Alliance.Blue),
-          new RegressionTest("Subsystem Test", TeleopScenario.AUTO_ROTATE, Alliance.Blue));
+          new RegressionTest("Auto test", AutoName.L_HALF_SWEEP_SHOOT, TeleopScenario.AUTO_ROTATE , Alliance.Blue));
       case SUBSYSTEM_TEST_TELE -> List.of(
           new RegressionTest("Subsystem Test", TeleopScenario.SUBSYSTEM_TEST, Alliance.Blue));
       case TEST_AUTOROTATE -> List.of(
