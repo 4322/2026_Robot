8586e708270c52d174d146b1af6b27aad535f6a4
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-22 08:12:23 -0700

    Disable untested autos in autonomousselector

--- src/main/java/frc/robot/autonomous/AutonomousSelector.java
+++ src/main/java/frc/robot/autonomous/AutonomousSelector.java
@@ -5,15 +5,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.autonomous.modes.CDepotOutpost;
 import frc.robot.autonomous.modes.DoNothing;
-import frc.robot.autonomous.modes.L2Sweep;
 import frc.robot.autonomous.modes.LHalfSweepShoot;
-import frc.robot.autonomous.modes.R2Sweep;
 import frc.robot.autonomous.modes.RDisruptSweepShoot;
 import frc.robot.autonomous.modes.RFullSweepShoot;
 import frc.robot.autonomous.modes.RHalfSuperSweepShoot;
 import frc.robot.autonomous.modes.RHalfSweepShoot;
 import frc.robot.autonomous.modes.RMidlineSweepShoot;
-import frc.robot.autonomous.modes.RROutpost;
 import frc.robot.autonomous.modes.RSweepBump;
 import frc.robot.commands.DriveCommands;
 import frc.robot.constants.Constants;
@@ -101,10 +98,7 @@ public class AutonomousSelector {
             new Auto(
                 AutoName.L_HALF_SWEEP_SHOOT,
                 new LHalfSweepShoot(drive, led, intake, shooter, hood)),
-            new Auto(AutoName.R_2_SWEEP, new R2Sweep(drive, led, intake, shooter)),
-            new Auto(AutoName.L_2_SWEEP, new L2Sweep(drive, led, intake, shooter)),
             new Auto(AutoName.R_SWEEP_BUMP, new RSweepBump(drive, led, intake, shooter)),
-            new Auto(AutoName.R_ROUTPOST, new RROutpost(drive, led, intake, shooter)),
             new Auto(
                 AutoName.DRIVE_WHEEL_RADIUS_CHARACTERIZATION,
                 new SequentialCommandGroup(
