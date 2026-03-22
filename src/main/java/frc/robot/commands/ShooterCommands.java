95248ae2b7845236dfd83dba29af67bc637aa4b4
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-22 11:41:21 -0700

    build

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -1,11 +1,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.drive.Drive;
