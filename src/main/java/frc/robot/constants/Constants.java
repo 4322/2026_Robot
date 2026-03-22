afb45b01ef5e006c5df533cbcf19da0c9c0e83df
Ryan Yeung <ryanyeung@frc4322.com>
2026-03-21 18:42:17 -0700

    Change unjam length to 0.5 seconds

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -504,7 +504,7 @@ public final class Constants {
 
     public static final double pathPlannerDrivekP = 5; // TODO probably increase
     public static final double pathPlannerRotationkP = 4;
-    public static final double unjamTimeSec = 2.0;
+    public static final double unjamTimeSec = 0.5;
   }
 
   public static class LED {
