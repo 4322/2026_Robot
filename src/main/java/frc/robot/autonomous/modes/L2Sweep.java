2b8de40d6e527857e1dfe0a85d7b28dd263993e8
Dhrish Karangula <dhrishkarangula@frc4322.com>
2026-03-29 21:30:47 -0700

    add preset hood angle for optimization

--- src/main/java/frc/robot/autonomous/modes/L2Sweep.java
+++ src/main/java/frc/robot/autonomous/modes/L2Sweep.java
@@ -40,7 +40,7 @@ public class L2Sweep extends SequentialCommandGroup {
         IntakeCommands.intake(intake),
         AutoBuilder.followPath(Robot.L_2SWEEP_A),
         new ParallelRaceGroup(
-            ShooterCommands.idle(shooter, intake), AutoBuilder.followPath(Robot.L_2SWEEP_B)),
+            ShooterCommands.idle(shooter, intake, 14.0), AutoBuilder.followPath(Robot.L_2SWEEP_B)),
         new ParallelCommandGroup(
             new ParallelRaceGroup(
                 ShooterCommands.autoShootNoAreaCheck(shooter, drive, intake),
