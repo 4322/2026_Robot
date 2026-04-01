77ea4e2367e5eb3af16ae0d5744dfaf1274f69bf
Ellie Morse <elliemorse@frc4322.com>
2026-03-31 20:37:14 -0700

    Hood Sim create queue

--- src/main/java/frc/robot/subsystems/shooter/hood/HoodIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/hood/HoodIOSim.java
@@ -1,6 +1,14 @@
 package frc.robot.subsystems.shooter.hood;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Timer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.LinkedBlockingDeque;
+
 import org.littletonrobotics.junction.Logger;
 
 public class HoodIOSim implements HoodIO {
@@ -10,6 +18,7 @@ public class HoodIOSim implements HoodIO {
   private double maxRPS = 0.2;
   private double degreePerSecond = 37;
   private double prevPosition = 0;
+  public Queue<Producer> positionQueue = new LinkedList<>();
 
   @Override
   public void updateInputs(HoodIOInputs inputs) {
@@ -28,9 +37,7 @@ public class HoodIOSim implements HoodIO {
 
   @Override
   public void simEstimatedPosition() {
-    this.position =
-        MathUtil.clamp((this.maxRPS * this.degreePerSecond * this.velocity), 0, 38) + prevPosition;
-    prevPosition = position;
+   
   }
 
   @Override
@@ -38,4 +45,44 @@ public class HoodIOSim implements HoodIO {
     // TODO
     Logger.recordOutput("Shooter/Hood/pulseWidth", pulseWidth);
   }
+
+  public class Producer {
+    private double offeredPosition;
+    private double timeAdded;
+    public Producer(double offeredPosition, double timeAdded) {
+      this.offeredPosition = offeredPosition;
+      this.timeAdded = timeAdded;
+    }
+    public void addPosition(double offeredPosition) {
+      timeAdded = Timer.getFPGATimestamp();
+      positionQueue.offer(new Producer(offeredPosition, timeAdded));
+    }
+   
+  }
+
+
+   public class Consumer {
+    private double timerCurrentValue;
+    private double requestedPosition;
+    public Consumer(double timerCurrentValue, double requestedPosition) {
+      this.timerCurrentValue = timerCurrentValue;
+      this.requestedPosition = requestedPosition;
+    }
+
+    public void checkPosition() {
+      if (positionQueue.isEmpty()) {
+        return;
+      }
+      timerCurrentValue = (Timer.getFPGATimestamp() - positionQueue.peek().timeAdded);
+      if (timerCurrentValue >= 0.120) {
+        requestedPosition = positionQueue.poll().offeredPosition;
+     }
+    }
+
+    public double getRequestedPosition() {
+      return requestedPosition;
+    }
+  }
+
+
 }
