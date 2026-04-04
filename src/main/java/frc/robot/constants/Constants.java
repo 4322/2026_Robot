70652dea7e3164b7ef3faa018f556297fa4a3967
Doug Kavner <dougkavner@frc4322.com>
2026-04-04 10:41:44 -0700

    Mark down bad camera

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -58,7 +58,7 @@ public final class Constants {
   public static final SubsystemMode firingManagerMode = SubsystemMode.NORMAL;
   public static final boolean turretLocked = false;
   public static boolean shootOnTheMoveEnabled = false;
-  public static final boolean frontRightCameraEnable = true;
+  public static final boolean frontRightCameraEnable = false;
   public static final boolean frontLeftCameraEnable = true;
   public static final boolean backRightCameraEnable = true;
   public static final boolean backLeftCameraEnable = true;
