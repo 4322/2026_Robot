78bb04588357ac3884556278c11b94b1a4cb484b
Ryan Yeung <ryanyeung@frc4322.com>
2026-04-05 21:06:44 -0700

    Add unjam to L/R2 Sweep

--- src/main/java/frc/robot/autonomous/modes/L2Sweep.java
+++ src/main/java/frc/robot/autonomous/modes/L2Sweep.java
@@ -38,7 +38,8 @@ public class L2Sweep extends SequentialCommandGroup {
         AutoBuilder.followPath(Robot.L_2SWEEP_A),
         new ParallelDeadlineGroup(
             AutoBuilder.followPath(Robot.L_2SWEEP_B),
-            ShooterCommands.idle(shooter, intake, 15.0, 40.0)),
+            ShooterCommands.idle(shooter, intake, 15.0, 40.0),
+            ShooterCommands.autoUnjam(shooter, Constants.Autonomous.unjamTimeSec)),
         new ParallelDeadlineGroup(
             AutoBuilder.followPath(Robot.L_2SWEEP_CG),
             ShooterCommands.autoShootNoAreaCheck(shooter, drive, intake),
@@ -51,7 +52,8 @@ public class L2Sweep extends SequentialCommandGroup {
         AutoBuilder.followPath(Robot.L_2SWEEP_DE),
         new ParallelDeadlineGroup(
             AutoBuilder.followPath(Robot.L_2SWEEP_F),
-            ShooterCommands.idle(shooter, intake, 14.0, 40.0)),
+            ShooterCommands.idle(shooter, intake, 14.0, 40.0),
+            ShooterCommands.autoUnjam(shooter, Constants.Autonomous.unjamTimeSec)),
         new ParallelCommandGroup(
             ShooterCommands.autoShootNoAreaCheck(shooter, drive, intake),
             IntakeCommands.autoSmoosh(
