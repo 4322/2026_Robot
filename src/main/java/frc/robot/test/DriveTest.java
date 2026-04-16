07af142593f22369179e2332965f1f165fbd670c
Ellie Morse <elliemorse@frc4322.com>
2026-04-15 22:13:33 -0700

    Build fixes

--- src/main/java/frc/robot/test/DriveTest.java
+++ src/main/java/frc/robot/test/DriveTest.java
@@ -3,15 +3,16 @@ package frc.robot.test;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.DriveCommands;
 import frc.robot.subsystems.drive.Drive;
 import org.littletonrobotics.junction.Logger;
 
 public class DriveTest extends SequentialCommandGroup {
-  Module[] modules;
 
-  public DriveTest(Drive drive) {
+  public DriveTest(Drive drive, DriveCommands driveCommands) {
     setName("DriveTest");
 
     addCommands(
@@ -21,17 +22,18 @@ public class DriveTest extends SequentialCommandGroup {
                 drive.runVelocity(
                     ChassisSpeeds.fromFieldRelativeSpeeds(
                         drive.getMaxLinearSpeedMetersPerSec(), 0, 0, Rotation2d.fromDegrees(0)))),
-        // new WaitUntilCommand(() -> drive.isDriveConnected()),
+        new ParallelCommandGroup(driveCommands.TesterDrive(drive), new WaitCommand(5)),
         new InstantCommand(
             () ->
                 drive.runVelocity(
                     ChassisSpeeds.fromFieldRelativeSpeeds(
                         drive.getMaxLinearSpeedMetersPerSec(), 0, 0, Rotation2d.fromDegrees(0)))),
-        new WaitCommand(5),
+        new ParallelCommandGroup(driveCommands.TesterDrive(drive), new WaitCommand(5)),
         new InstantCommand(
             () ->
                 drive.runVelocity(
                     ChassisSpeeds.fromFieldRelativeSpeeds(
-                        drive.getMaxLinearSpeedMetersPerSec(), 0, 0, Rotation2d.fromDegrees(0)))));
+                        drive.getMaxLinearSpeedMetersPerSec(), 0, 0, Rotation2d.fromDegrees(0)))),
+        new ParallelCommandGroup(driveCommands.TesterDrive(drive), new WaitCommand(5)));
   }
 }
