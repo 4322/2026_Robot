123debd701e4befdacb3b36b76b1957b4fe38e83
Ellie Morse <elliemorse@frc4322.com>
2026-04-16 21:43:35 -0700

    Constant

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -252,7 +252,7 @@ public final class Constants {
     public static final int idleTimeout = 0;
 
     public static final double kV = 0;
-    public static final double kP = 0;
+    public static final double kP = 1.0;
     public static final double kD = 0;
     public static final double kI = 0;
     public static final double kS = 0;
