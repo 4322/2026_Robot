c29a4cbbd3d0f4d38ee04625b82f777721e53e47
Ryan Yeung <ryanyeung@frc4322.com>
2026-04-21 15:51:59 -0700

    Log odometry on nt tables for elastic

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -37,6 +37,8 @@ import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -100,6 +102,8 @@ public class Drive extends SubsystemBase {
       };
   private SwerveDrivePoseEstimator poseEstimator =
       new SwerveDrivePoseEstimator(kinematics, rawGyroRotation, lastModulePositions, Pose2d.kZero);
+
+  private Field2d fieldPosition = new Field2d();
 
   public Drive(
       GyroIO gyroIO,
@@ -214,6 +218,9 @@ public class Drive extends SubsystemBase {
     if (Constants.debugZoneAreas) {
       RobotContainer.getField().setRobotPose(getRobotPose());
     }
+
+    fieldPosition.setRobotPose(getRobotPose());
+    SmartDashboard.putData(fieldPosition);
   }
 
   /**
