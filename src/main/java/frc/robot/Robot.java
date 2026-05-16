76633e123e2c34ff80f61affffc6190404f9a650
Ryan Yeung <ryanyeung@frc4322.com>
2026-05-15 19:38:50 -0700

    Move zeroing to povLeft

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -410,14 +410,11 @@ public class Robot extends LoggedRobot {
         // button is pressed in
         DriverStation.reportWarning("Activating Coast Mode", false);
         coastButtonTimer.start();
-        RobotContainer.drive.setPose(new Pose2d(new Translation2d(3.6, 4), Rotation2d.k180deg));
       }
       if (coastButtonTimer.hasElapsed(0.1)) {
-        RobotContainer.drive.setPose(new Pose2d(new Translation2d(3.6, 4), Rotation2d.k180deg));
         robotContainer.setBrakeMode(false);
       }
       if (coastButtonTimer.hasElapsed(10)) {
-        RobotContainer.drive.setPose(new Pose2d(new Translation2d(3.6, 4), Rotation2d.k180deg));
         DriverStation.reportWarning("Activating Brake Mode", false);
         robotContainer.setBrakeMode(true);
         coastButtonTimer.stop();
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,15 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.autonomous.AutonomousSelector;
 import frc.robot.commands.DriveCommands;
@@ -388,6 +392,8 @@ public class RobotContainer {
     controller.y().onTrue(IntakeCommands.smoosh(intake)).onFalse(IntakeCommands.toggleOff(intake));
     controller.rightBumper().onTrue(ShooterCommands.turretUnjamOverride(shooter, true));
     controller.rightBumper().onFalse(ShooterCommands.turretUnjamOverride(shooter, false));
+
+    controller.povLeft().onTrue(new InstantCommand(() -> drive.setPose(new Pose2d(new Translation2d(3.6, 4), Rotation2d.k180deg))));
   }
 
   /**
