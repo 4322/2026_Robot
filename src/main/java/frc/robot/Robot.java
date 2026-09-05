9ab14be276258ec80cf93267f10c0feb81b08352
Ryan Yeung <ryanyeung@frc4322.com>
2026-09-05 14:14:35 -0700

    Formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -477,8 +477,7 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {
-  }
+  public void simulationPeriodic() {}
 
   public static void stabilize() {
     stabilizeCamerasTimer.start();
